c938ffa5336b979e0f3fab19cf526c35c0618be2
christy cooper betty <ccooper.prsvr@gmail.com>
2023-11-28 11:11:27 -0600

    finished work

--- main.js
+++ main.js
@@ -1,10 +1,49 @@
 /******************************
  * YOUR CODE GOES RIGHT HERE! *
  ******************************/
+function isItLong(str){
+ if (str.length > 20){
+  return "That's a long string!"
+ }
+}
 
+  function isItMedium(str) {
+    if (str.length >= 10 && str.length <= 20) {
+      return "That's a regular sized string!"
+    }
+  }
 
+  function isItShort(str) {
+    if (str.length < 10) {
+      return "That's a small string!"
+    } else {
+      return "That's not a small string!"
+    }
+  }
 
+  function howLongIsMyString(str){
+    if (str.length > 20) {
+      return "That's a long string!"
+    } else if (str.length >= 10 && str.length <= 20) {
+      return "That's a regular sized string!"
+    } else if (str.length < 10){
+      return "That's a small string!"
+     }
+    }
+  
+    function personsHeight(str){
+      if (str === "Shaq"){
+        return  85
+      }else if (str === "Zooey"){
+        return 65
+      } else if (str !== "Shaq" || "Zooey"){
+        return "I don't know everyone's height!"
+      }
+    }
 
+    
+
+    
 /**********************************
  *      OUR CODE GOES BELOW.      *
  * THIS IS FOR INTERNAL USE ONLY. *
